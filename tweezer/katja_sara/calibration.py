7f3c99b0fd37563bcbd47fe72f7e1ad01157100b
Sara Marinček <sarapia.marincek@gmail.com>
2019-03-14 17:11:17 +0100

    Popravila rotacijo elipse.

--- tweezer/katja_sara/calibration.py
+++ tweezer/katja_sara/calibration.py
@@ -50,102 +50,45 @@ def center_and_rotate(xdata, ydata):
     def center(data):
         return data - np.mean(data)
 
-    xdata_centered = center(xdata)
-    ydata_centered = center(ydata)
+    def rotate(x, y):
+        n = len(x)
 
-    def linear(x, k):
-        return k*x
+        cov = np.cov(x, y)
+        var, vec = np.linalg.eigh(cov)
+        phi = -np.arctan(vec[:, 1][1]/vec[:, 1][0])
 
-    k, _ = scipy.optimize.curve_fit(
-        linear, xdata_centered, ydata_centered
-    )
-    phi = -np.arctan(*k)
+        def rotate_one(phi, x, y):
+            x_rotated = np.cos(phi)*x - np.sin(phi)*y
+            y_rotated = np.sin(phi)*x + np.cos(phi)*y
+            return x_rotated, y_rotated
 
-    def rotate(phi, x, y):
-        x_rotated = np.cos(phi)*x - np.sin(phi)*y
-        y_rotated = np.sin(phi)*x + np.cos(phi)*y
-        return x_rotated, y_rotated
+        x_rotated, y_rotated = (np.zeros(n), np.zeros(n))
+        for i in range(n):
+            x_rotated[i], y_rotated[i] = rotate_one(
+                phi, x[i], y[i]
+            )
+        return x_rotated, y_rotated, phi, var[::-1]
 
-    xy_rotated = np.zeros((len(xdata), 2))
-    for i in range(len(xy_rotated)):
-        xy_rotated[i] = rotate(
-            phi, xdata_centered[i], ydata_centered[i]
-        )
-
-    return xy_rotated[:, 0], xy_rotated[:, 1], phi
-
-
-def histogram_and_fit_gaussian(data):
-    """
-    Calibrates by histogramming position deviations
-    (into int(np.ceil(np.sqrt(len(data)))) bins)
-    by fitting a gaussian function
-    y = prefactor e^(-x^2/(2 variance)).
-
-    Args:
-        data: Array of position deviations in one dimension.
-
-    Returns:
-        Centres of histogram bins,
-        heights of bins,
-        prefactor,
-        variance.
-    """
-
-    heights, bin_edges = np.histogram(
-        data, bins=int(np.ceil(np.sqrt(len(data)))),
-        density=True
-    )
-    bin_centres = (bin_edges[:-1] + bin_edges[1:])/2.
-
-    def gaussian(x, prefactor, variance):
-        return prefactor*np.exp(-x**2./(2.*variance))
-
-    (prefactor, variance), _ = scipy.optimize.curve_fit(
-        gaussian, bin_centres, heights,
-        p0=[1., np.var(data)]
-    )
-
-    return bin_centres, heights, prefactor, variance
+    return rotate(center(xdata), center(ydata))
 
 
 def calibrate(time, xdata, ydata, temp=293):
-    """
-    Calibrates a tweezer using histogram_and_fit_gaussian.
-
-    Args:
-        xdata: Array of x-positions.
-        ydata: Array of y-positions.
-
-    Returns:
-        (k_x, k_y),
-        angle of rotation in the anticlockwise direction.
-    """
 
     x = subtract_linear_drift(time, xdata)
     y = subtract_linear_drift(time, ydata)
-    x, y, phi = center_and_rotate(x, y)
 
-    xy = np.zeros((len(xdata), 2))
-    xy[:, 0] = x
-    xy[:, 1] = y
-    ks = []
+    x, y, phi, var = center_and_rotate(x, y)
+    k = KB*temp/var*1e12
 
-    for i in range(2):
-        _, _, _, variance = histogram_and_fit_gaussian(
-            xy[:, i]
-        )
-        k = KB*temp/variance*1e12
-        ks.append(k)
-
-    return tuple(ks), phi
+    return tuple(k), phi
 
 
 def plot(time, xdata, ydata, temp=293):
 
     x = subtract_linear_drift(time, xdata)
     y = subtract_linear_drift(time, ydata)
-    x, y, phi = center_and_rotate(x, y)
+    x, y, phi, var = center_and_rotate(x, y)
+    k = KB*temp/var*1e12
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
     ax1.set_title('Original data')
@@ -161,7 +104,7 @@ def plot(time, xdata, ydata, temp=293):
     fig.tight_layout()
     plt.show()
 
-    xy = np.zeros((len(xdata), 2))
+    xy = np.zeros((len(x), 2))
     xy[:, 0] = x
     xy[:, 1] = y
 
@@ -171,15 +114,16 @@ def plot(time, xdata, ydata, temp=293):
         ax = fig.add_subplot(1, 2, i+1)
         ax.set_xlabel('x [10^(-6) m]')
         ax.set_ylabel('Bin height')
-        bin_centres, heights, prefactor, variance = histogram_and_fit_gaussian(
-            xy[:, i]
+        hist, bin_edges = np.histogram(
+            xy[:, i], bins=int(np.sqrt(len(x))), density=True
         )
-        k = KB*temp/variance*1e12
         ax.set_title('k{} = {:.2e}J/m^2'.format(
-            titles[i], k))
-        ax.scatter(bin_centres, heights, s=4)
+            titles[i], k[i]))
+        bin_centres = (bin_edges[:-1] + bin_edges[1:])/2.
+        ax.scatter(bin_centres, hist, s=4)
         x_model = np.linspace(min(bin_centres), max(bin_centres), 100)
-        ax.plot(x_model, prefactor*np.exp(-x_model**2./(2.*variance)))
+        prefactor = 1./np.sqrt(2.*np.pi*var[i])
+        ax.plot(x_model, prefactor*np.exp(-x_model**2./(2.*var[i])))
     fig.tight_layout()
     plt.show()
 
